d3b9463ecb56ee80953e69b80372ddd2bbcdc681
logvinovdan <76569554+logvinovdan@users.noreply.github.com>
2023-04-07 01:17:44 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,13 +1,11 @@
 import pandas as pd
 import numpy as np
 
-from scipy.stats import norm
-
-
 chat_id = 834639322 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
-    b = 2 * x - 0.083
-    b_up = np.quantile(b, p)
-    b_down = 2 * b.mean() - b_up
-    return b_down, b_up
+    alpha = p
+    p_values = x
+    q = np.quantile(p_values, alpha)
+    confidence_interval = [0.083, q]
+    return confidence_interval[1], confidence_interval[0]
